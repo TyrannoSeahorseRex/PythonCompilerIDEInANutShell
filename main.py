ba054b63ea640735119ebb351a7edbe80c473726
Sea Rex <wheeler_harry@icloud.com>
2025-01-29 19:26:33 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from tkinter import *
 from tkinter.filedialog import asksaveasfilename
 from tkinter.filedialog import askopenfilename
 import subprocess
+
+compiler = Tk()
+compiler.title('IDE IN A NUT-SHELL 🌰')
