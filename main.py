d3f9b65ef5dabc5739253a043355a1df783ade4c
Sea Rex <wheeler_harry@icloud.com>
2025-01-29 19:27:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,15 @@ file_path = ''
 def set_file_path(path):
   global file_path
   file_path = path
+
+def run():
+  if file_path == '':
+    save_prompt = Toplevel()
+    text = Label(save_prompt, text='Please save your code before attempting to run it :)')
+    text.pack()
+    return
+  command = f'python {file_path}'
+  process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+  output, error = process.communicate()
+  code_output.insert(END, output)
+  code_output.insert(END, error)
